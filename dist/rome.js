f46569adfe365551b24e63695d3de45a25d641f5
root <root@ns513780.ip-167-114-158.net>
2019-02-11 18:29:11 +0000

    Added try/catch to unbound() function to avoid throwing a fatal error in old browsers like IE8

--- dist/rome.js
+++ dist/rome.js
@@ -283,23 +283,24 @@ function throttle (fn, boundary) {
   var last = -Infinity;
   var timer;
   return function bounced () {
+    function unbound () {
+      try{
+        clearTimeout(timer);
+        timer = null;
+        var next = last + boundary;
+        var now = Date.now();
+        if (now > next) {
+          last = now;
+          fn();
+        } else {
+          timer = setTimeout(unbound, next - now);
+        }
+      }catch(e){}
+    }
     if (timer) {
       return;
     }
     unbound();
-
-    function unbound () {
-      clearTimeout(timer);
-      timer = null;
-      var next = last + boundary;
-      var now = Date.now();
-      if (now > next) {
-        last = now;
-        fn();
-      } else {
-        timer = setTimeout(unbound, next - now);
-      }
-    }
   };
 }
 
@@ -5794,23 +5795,24 @@ module.exports = function throttle (fn, boundary) {
   var last = -Infinity;
   var timer;
   return function bounced () {
+    function unbound () {
+      try{
+        clearTimeout(timer);
+        timer = null;
+        var next = last + boundary;
+        var now = +new Date();
+        if (now > next) {
+          last = now;
+          fn.apply(this, arguments);
+        } else {
+          timer = setTimeout(unbound, next - now);
+        }
+      }catch(e){}
+    }
     if (timer) {
       return;
     }
     unbound();
-
-    function unbound () {
-      clearTimeout(timer);
-      timer = null;
-      var next = last + boundary;
-      var now = +new Date();
-      if (now > next) {
-        last = now;
-        fn.apply(this, arguments);
-      } else {
-        timer = setTimeout(unbound, next - now);
-      }
-    }
   };
 };
 
--- src/throttle.js
+++ src/throttle.js
@@ -4,22 +4,23 @@ module.exports = function throttle (fn, boundary) {
   var last = -Infinity;
   var timer;
   return function bounced () {
+    function unbound () {
+      try{
+        clearTimeout(timer);
+        timer = null;
+        var next = last + boundary;
+        var now = +new Date();
+        if (now > next) {
+          last = now;
+          fn.apply(this, arguments);
+        } else {
+          timer = setTimeout(unbound, next - now);
+        }
+      }catch(e){}
+    }
     if (timer) {
       return;
     }
     unbound();
-
-    function unbound () {
-      clearTimeout(timer);
-      timer = null;
-      var next = last + boundary;
-      var now = +new Date();
-      if (now > next) {
-        last = now;
-        fn.apply(this, arguments);
-      } else {
-        timer = setTimeout(unbound, next - now);
-      }
-    }
   };
 };
